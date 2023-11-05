49062bdd803a8e1f3dd5af9a5e3bda77d7a5e225
marlonholguing <mlholguing@unal.edu.co>
2023-11-05 00:40:32 -0500

    navegacion funcionando

--- Navigation.js
+++ Navigation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 
 //vistas
@@ -7,17 +8,37 @@ import Bodega from "./Pantallas/Bodega"
 import Cocina from "./Pantallas/Cocina"
 import Inicio from "./Pantallas/Inicio"
 import Ordenes from "./Pantallas/Ordenes";
-import StakScreen from "./Pantallas/StackScreen"
 import Usuarios from "./Pantallas/Usuario"
+import { ScreenStackHeaderSearchBarView } from "react-native-screens";
 
-//constante para el butomtabnavigator
+const HomeStack =createNativeStackNavigator();
+function MyStack(){
+    return(
+        <HomeStack.Navigator
+        initialRouteName="PantallaInicio">
+            <HomeStack.Screen
+                name="PantallaInicio"
+                component={Inicio}
+                options={{ headerShown: false }}
+            />
+            <HomeStack.Screen
+                name="Ordenes"
+                component={Ordenes}
+                options={{ headerShown: false }}
+            />
+        </HomeStack.Navigator>
+    );
+}
+
+
+
 const Tab =createBottomTabNavigator();
 function MyTabs(){
 
     return(
         <Tab.Navigator
         initialRouteName="Inicio">
-            <Tab.Screen name ="Inicio" component={Inicio} />
+            <Tab.Screen name ="Inicio" component={MyStack} />
             <Tab.Screen name ="Ordenes" component={Ordenes} />
             <Tab.Screen name ="Cocina" component={Cocina} />
             <Tab.Screen name ="Bodega" component={Bodega} />
